6ca9e487c29170c4da55fcc9ca2cd9a392586f02
Adam Brows <aabrowsbrows@gmail.com>
2024-10-21 16:51:26 +0100

    com

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -2,6 +2,7 @@ package catalogue;
 
 import java.io.Serializable;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Write a description of class BetterBasket here.
@@ -13,7 +14,33 @@ public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
 
-  
+  public BetterBasket() { 
+
+  } 
+
+  List<Basket> list; 
+
+  //Collections.sort(list){	 
+
+  //} 
+
+   
+
+  public int compareTo(Product o) { 
+
+	  int result = o.getProductNum().compareToIgnoreCase(o.getProductNum()); 
+
+	  if(result !=0) { 
+
+	  return result; 
+
+  }else { 
+
+	  return 0; 
+
+  } 
+
+  } 
   
 
   // You need to add code here
