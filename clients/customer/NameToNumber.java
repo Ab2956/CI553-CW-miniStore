f41812f8aa03e3f3c18700fc01f55954c4afc789
Adam Brows <aabrowsbrows@gmail.com>
2024-11-04 20:17:55 +0000

    com

--- clients/customer/NameToNumber.java
+++ clients/customer/NameToNumber.java
@@ -1,1 +1,16 @@
+package clients.customer;
+import java.util.HashMap;
 
+public class NameToNumber extends HashMap<String, String>{
+	
+	NameToNumber(){
+		
+		put("0001","TV");
+		put("0002","Radio");
+		put("0003","Toaster");
+		put("0004","Watch");
+		put("0005","Camera");
+		put("0006","Music player");
+		put("0007","USB driver");
+	}
+}
