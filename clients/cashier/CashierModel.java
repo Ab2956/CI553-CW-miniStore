250e69eb6712cc1a321905a414c92425cd6349de
Adam Brows <aabrowsbrows@gmail.com>
2024-11-25 17:02:58 +0000

    com

--- clients/cashier/CashierModel.java
+++ clients/cashier/CashierModel.java
@@ -163,7 +163,7 @@ public class CashierModel extends Observable
 	  if(theBasket != null && !theBasket.isEmpty()) {						// if statement to see if the basket is empty
 		  for(Product p : theBasket) {										// enhanced for loop to iterate through the basket
 			 int currentBasketQuantity = theBasket.getQuantityBasket(p);	// let currentBasketQuatity equals getQuantityBasket
-			  theBasket.setQuantityBasket(p, currentBasketQuantity +1);		// add 1 every time a product is counted 
+			  theBasket.setQuantityBasket(p, currentBasketQuantity + p.getQuantity());		// add quantity every time a product is counted 
 			 
 		  }
 		  theStock.addStock(theProduct.getProductNum(), theBasket.getQuantityBasket(theProduct)); // add Stock back to its original count
