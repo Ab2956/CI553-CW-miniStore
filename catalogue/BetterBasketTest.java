861e4b52d5237f34925d58d958fb6d368030dc8a
Adam Brows <aabrowsbrows@gmail.com>
2024-12-16 19:27:11 +0000

    test

--- catalogue/BetterBasketTest.java
+++ catalogue/BetterBasketTest.java
@@ -10,7 +10,7 @@ class BetterBasketTest {
 
 	@Test
 	void testMergeAddProduct() {
-		ArrayList<Product> br = new ArrayList<>();
+		BetterBasket br = new BetterBasket();
 		Product p1 = new Product("0001","TV",12.3,1);
 		Product p2 = new Product("0002","Radio",13.4,1);
 		Product p3 = new Product("0003","Toaster",10.5,1);
@@ -20,8 +20,8 @@ class BetterBasketTest {
 		br.add(p1);
 		br.add(p2);
 		br.add(p2);
-		assertEquals(6,br.size(),"error: merge");
-		assertEquals(1,br.get(1).getQuantity(),"error: quantity");
+		assertEquals(3,br.size(),"error: merge");
+		assertEquals(2,br.get(0).getQuantity(),"error: quantity");
 	}
 
 }
