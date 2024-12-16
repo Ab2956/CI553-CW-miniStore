c81482161d108e77ce025a893dff37b725e9587f
Adam Brows <aabrowsbrows@gmail.com>
2024-12-16 19:34:54 +0000

    test

--- catalogue/BetterBasketTest.java
+++ catalogue/BetterBasketTest.java
@@ -23,5 +23,20 @@ class BetterBasketTest {
 		assertEquals(3,br.size(),"error: merge");
 		assertEquals(2,br.get(0).getQuantity(),"error: quantity");
 	}
+	
+	@Test
+	void  testSortProduct() {
+		BetterBasket br = new BetterBasket();
+		Product p1 = new Product("0001","TV",12.3,1);
+		Product p2 = new Product("0002","Radio",13.4,1);
+		Product p3 = new Product("0003","Toaster",10.5,1);
+		br.add(p1);
+		br.add(p3);
+		br.add(p2);
+		assertEquals("0001", br.get(0).getProductNum(), "error: sort");
+	}
+		
+		
+
 
 }
